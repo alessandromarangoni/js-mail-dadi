eb616ac606d52d0880282a29ed7889e1fa1a5835
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-04-18 16:10:15 +0200

    penso di aver finito

--- main.js
+++ main.js
@@ -1,8 +1,19 @@
-const mail = prompt("mail")
-const users = ["mariorossi@gmail.com", "marioverdi@gmail.com", "mariogialli@gmail.com", "marioneri@gmail.com", "mariobianchi@gmail.com","marioazzurri@gmail.com"]
-for (let c = 0; c < users.length; c++) {
-    console.log(users[c]);
-    if(mail == users[c]){
-        alert("ok")
-    }
+    const bottone = document.getElementById("bottone")
+    bottone.addEventListener("click", function(){
+    const mail = document.getElementById("mail").value
+    const users = ["mariorossi@gmail.com", "marioverdi@gmail.com", "mariogialli@gmail.com", "marioneri@gmail.com", "mariobianchi@gmail.com","marioazzurri@gmail.com"]
+
+    for (let c = 0; c < users.length; c++) {
+        console.log(users[c]);
+        if(mail == users[c]){
+            var risultato = ("login effettuato")
+
+        }
+        else{
+            var risultato = ("mail non trovata registrarsi prego")
+        }
+        let print = document.getElementById("span")
+        print.innerHTML =`${risultato}`
 }
+    }
+)
