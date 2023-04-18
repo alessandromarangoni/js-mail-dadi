cb6a2c71306e849985b5c1fddca8260ba636f3f8
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-04-18 16:37:05 +0200

    con l alert funziona

--- main.js
+++ main.js
@@ -1,19 +1,19 @@
-    const bottone = document.getElementById("bottone")
-    bottone.addEventListener("click", function(){
-    const mail = document.getElementById("mail").value
-    const users = ["mariorossi@gmail.com", "marioverdi@gmail.com", "mariogialli@gmail.com", "marioneri@gmail.com", "mariobianchi@gmail.com","marioazzurri@gmail.com"]
+const bottone = document.getElementById("bottone")
+bottone.addEventListener("click", function(){
+const mail = document.getElementById("mail").value
+var users = ["mariorossi@gmail.com", "marioverdi@gmail.com", "mariogialli@gmail.com", "marioneri@gmail.com", "mariobianchi@gmail.com","marioazzurri@gmail.com"]
 
-    for (let c = 0; c < users.length; c++) {
-        console.log(users[c]);
-        if(mail == users[c]){
-            var risultato = ("login effettuato")
-
-        }
-        else{
-            var risultato = ("mail non trovata registrarsi")
-        }
-        let print = document.getElementById("span")
-        print.innerHTML =`${risultato}`
-}
+for (let c = 0; c < users.length; c++) {
+    console.log(users[c]);
+    if(mail == users[c]){
+        alert("ok")
+        
     }
+    else{
+        alert("no")
+    }
+    // let print = document.getElementById("span")
+    // print.innerHTML =`${risultato}`
+    }
+}
 )
