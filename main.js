3f80e19c2584920b81148cca455a0909c4f2d070
alessandro marangoni <alessandromarangoni980@gmail.com>
2023-04-18 16:59:56 +0200

    okk

--- main.js
+++ main.js
@@ -1,16 +1,19 @@
 const bottone = document.getElementById("bottone")
 bottone.addEventListener("click", function(){
 const mail = document.getElementById("mail").value
-var users = ["mariorossi@gmail.com", "marioverdi@gmail.com", "mariogialli@gmail.com", "marioneri@gmail.com", "mariobianchi@gmail.com","marioazzurri@gmail.com"]
+const users = ["mariorossi@gmail.com", "marioverdi@gmail.com", "mariogialli@gmail.com", "marioneri@gmail.com", "mariobianchi@gmail.com","marioazzurri@gmail.com"]
 
 for (let c = 0; c < users.length; c++) {
     console.log(users[c]);
     if(mail == users[c]){
-        alert("ok")
-        
+        const risultato = document.getElementById("risultato")
+
+        // alert("ok");
+        risultato.innerHTML="si"
+        break
     }
     else{
-        alert("no")
+        risultato.innerHTML="no"
     }
     // let print = document.getElementById("span")
     // print.innerHTML =`${risultato}`
